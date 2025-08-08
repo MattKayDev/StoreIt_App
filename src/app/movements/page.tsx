2b70a726b0dddcf654108a75580ca449aa62f1e2
Maciej “Matt” Kolacinski <maciek200895@gmail.com>
2025-08-08 21:59:10 +0000

    okay let's rename the app from Inventrack to StoreIt App

--- src/app/movements/page.tsx
+++ src/app/movements/page.tsx
@@ -114,7 +114,7 @@ export default function MovementsPage() {
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6 text-primary" />
-              <span className="">InvenTrack</span>
+              <span className="">StoreIt App</span>
             </Link>
           </div>
           <div className="flex-1">
@@ -181,7 +181,7 @@ export default function MovementsPage() {
                   className="flex items-center gap-2 text-lg font-semibold"
                 >
                   <Package2 className="h-6 w-6 text-primary" />
-                  <span className="sr-only">InvenTrack</span>
+                  <span className="sr-only">StoreIt App</span>
                 </Link>
                 <Link
                   href="/"
