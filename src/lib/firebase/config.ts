69c128f0a683f7b1298e9e126097cc7a37221bf5
Maciej “Matt” Kolacinski <maciek200895@gmail.com>
2025-08-08 22:09:29 +0000

    but embedding the firebase configuration in the applicaiton is bad right

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,13 +5,13 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+      apiKey: "your-api-key",
+      authDomain: "your-project-id.firebaseapp.com",
+      projectId: "your-project-id",
+      storageBucket: "your-project-id.appspot.com",
+      messagingSenderId: "your-sender-id",
+      appId: "your-app-id",
+      databaseURL: "https://your-project-id.firebaseio.com"
     };
 
 // Initialize Firebase
