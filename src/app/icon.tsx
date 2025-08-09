532beb4680cadcb0f5da6ca60d8671fd578e2abb
Maciej “Matt” Kolacinski <maciek200895@gmail.com>
2025-08-09 03:54:00 +0000

    great I like it much more but I wanted the icon to be much more like thi

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -28,24 +28,20 @@ export default function Icon() {
           borderRadius: '8px'
         }}
       >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
             strokeLinejoin="round"
         >
-            <path d="M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v2" />
-            <path d="M21 14v2a2 2 0 0 1-1 1.73l-7 4a2 2 0 0 1-2 0l-7-4A2 2 0 0 1 3 16v-2" />
-            <path d="M3 12v.01" />
-            <path d="M12 12v.01" />
-            <path d="M21 12v.01" />
-            <path d="m3.45 10.12 7.07-4.02a2 2 0 0 1 1.96 0l7.07 4.02" />
-            <path d="m20.55 13.88-7.07 4.02a2 2 0 0 1-1.96 0l-7.07-4.02" />
+            <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z"/>
+            <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9"/>
+            <path d="M12 3v6"/>
         </svg>
       </div>
     ),
