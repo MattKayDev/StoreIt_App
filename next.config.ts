dab57aac8e7e802b5c1a76e82a8e6021636636be
Maciej “Matt” Kolacinski <maciek200895@gmail.com>
2025-08-09 06:19:00 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 
 import type {NextConfig} from 'next';
+import withPWAInit from '@ducanh2912/next-pwa';
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -39,4 +41,11 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+});
+
+export default withPWA(nextConfig);
